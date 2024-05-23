06bd7f756360db270236b3291f660d5e831d0c79
frknozr <frknozr@gmail.com>
2024-05-23 21:50:43 +0300

    slides updated

--- fix-gpo.ps1
+++
@@ -1,4 +0,0 @@
-$domain = Get-ADDomain | Select-Object -ExpandProperty ParentDomain
-$domain_dn = Get-ADDomain | Select-Object -ExpandProperty DistinguishedName
-New-GPLink -Domain $domain -Name "Disable Security Features" -Target $domain_dn
-New-GPLink -Domain $domain -Name "Update Local Group Memberships" -Target $domain_dn
